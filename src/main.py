8958e2c3b9f8663aa26f7d015bfc85d36dcd06b1
Alexandra Gushchina <ge35xab@mytum.de>
2023-09-05 01:24:27 +0200

    new methods for the commands and docker support

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 
-import re
+import configparser
 import telegram
 
 from telegram import Update
@@ -7,27 +7,29 @@ from bot import *
 from telegram.ext import (
     Application,
     CommandHandler,
-   # ContextTypes,
-   # ConversationHandler,
     MessageHandler,
-   # filters,
+    filters,
 )
 
+config = configparser.ConfigParser()
+config.read('config.ini', encoding = 'utf-8-sig')
 
 #
 # Constants
 #
-BOT_TOKEN = "6210165185:AAFr9ATj95TXkbDC7pjrEklfDouTuIMw2NY"
+API_ID = config.get('default','API_ID')
+API_HASH = config.get('default','API_HASH')
+BOT_TOKEN = config.get('default','BOT_TOKEN')
 REVIEW, INFO = range(2)
 
 def main() -> None:
 
+  print('Starting bot...')
   # Initialize the telegram bot with our bot token
   application = Application.builder().token(BOT_TOKEN).build()
   
   #yes_no_regex = re.compile(r'^(да|нет|ja|nein)$', re.IGNORECASE)
 
-  # Create our ConversationHandler, with only one state
   handler = telegram.ext.ConversationHandler(
     entry_points = [CommandHandler('start', start),CommandHandler('faq', faq), CommandHandler('exit', exit)],
 
@@ -42,8 +44,13 @@ def main() -> None:
   
   application.add_handler(handler)
 
+  application.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+  application.add_error_handler(error)
+
   # Start the bot until the user presses Ctrl-C
-  application.run_polling(allowed_updates = Update.ALL_TYPES)
+  print('Polling bot...')
+  application.run_polling(poll_interval = 3, allowed_updates = Update.ALL_TYPES)
 
 if __name__ == '__main__':
   main()
